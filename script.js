7f0e9c4dc4c369452dc9e811f88bd66e191ac05f
La Panchoneta <168029728+LaPanchoneta@users.noreply.github.com>
2024-06-03 19:12:22 -0300

    agregamos un js

---
+++ script.js
@@ -0,0 +1,11 @@
+// crear una variable constante y con texto(2da linea de texto) y hacer como un "print" (3ra linea) - Uziel
+const text= "hola momeros";
+console.log(text) 
+
+// crea una variable constante y integra el texto y una comparacion (6ta linea de texto )- alexis
+console.log("esta pagina es visitada por mas de 50 mil personas por dia, y por mes: ", 50000 * 30);
+
+// creo una variable constante con el contenido new Date() para obtener la fecha y luego hice la variable msg para que siempre que se llame lo haga con un mensaje
+const fecha= new Date();
+const msg= ('ingreso a la pagina: '+ fecha)
+console.log( msg)
